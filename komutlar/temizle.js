afd4174ba84dc7e5a7a6aa367e3b84b1e6b6dd5e
TurkOglu1655 <42115393+TurkOglu1655@users.noreply.github.com>
2018-09-13 19:09:47 +0300

    Add files via upload

---
+++ komutlar/temizle.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+
+  if (!message.guild) {
+    return message.author.send('`temizle` komutu sadece sunucularda kullanılabilir.');
+  }
+  let mesajsayisi = args.slice(0).join(' ');
+  if (!mesajsayisi) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin.')
+  if (mesajsayisi > 100) return message.channel.send('100 adetden fazla mesaj silemem!');
+  message.channel.bulkDelete(mesajsayisi);
+  
+  message.channel.send("**Fısfıs** *" + mesajsayisi + "* **tane yazı temizledi ^^**").then(msg => {msg.delete(5000)});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sil', "temizle"],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'fısfıs',
+  description: 'Belirlenen miktar mesajı siler.',
+  usage: 'fısfıs <temizlenecek mesaj sayısı>'
+};
---
+++ komutlar/yenilikler.js
@@ -0,0 +1,30 @@
+﻿const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+    const embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setDescription("Yenilik her zaman iyidir")
+    .setThumbnail("http://fantasyva.com/wp-content/uploads/2017/08/new-sign-ID-33314.png")
+    .setColor(0x00AE86)
+    .addField("RPG (MOB) Oyunu Eklendi: ", "Artık botumuzun oluşturduğu moblarla savaşabileceksiniz -help rpg")
+.setDescription(`-başla Komutu ile moblarımızla savaşa başlayabilirsiniz! Kullanabileceğiniz saldırılar: saldır, ateştopu, can, em. -bitir Komutuyla savaşı bitirebilirsiniz (doğal olarak kaybedersiniz). Şuanda sadece eğlence amaçlıdır gelecekte para sistemiyle paranızı ikiye katlayabileceksiniz!`)
+    message.channel.send(embed);
+
+};
+
+
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'yenilikler', 
+  description: 'Yenilik her zaman iyidir',
+  usage: 'yenilikler'
+};
